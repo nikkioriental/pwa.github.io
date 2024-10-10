de919ee190c479a6f09816c1b0707ecf4429263a
n.davydov <nikitadavydov@MacBook-Pro-Nikita.local>
2024-10-10 19:02:06 +0800

    Added check that promt was shown

--- app.js
+++ app.js
@@ -1,41 +1,46 @@
 let deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', (e) => {
-  e.preventDefault();
-  deferredPrompt = e;
+    e.preventDefault();
+    deferredPrompt = e;
 
-  const installButton = document.getElementById('installButton');
-  installButton.style.display = 'none';
+    if (!localStorage.getItem('pwaInstallPromptShown')) {
+        const installButton = document.getElementById('installButton');
+        installButton.style.display = 'none';
 
-  const loadingDialog = document.getElementById('loadingDialog');
-  loadingDialog.style.display = 'block';
+        const loadingDialog = document.getElementById('loadingDialog');
+        loadingDialog.style.display = 'block';
 
-  setTimeout(() => {
-    loadingDialog.style.display = 'none';
-    const installButton = document.getElementById('installButton');
-    installButton.style.display = 'block';
-  }, 5000);
+        setTimeout(() => {
+            loadingDialog.style.display = 'none';
+            const installButton = document.getElementById('installButton');
+            installButton.style.display = 'block';
+        }, 5000);
+    }
+
+
 });
 
 document.getElementById('installButton').addEventListener('click', () => {
-  deferredPrompt.prompt();
+    deferredPrompt.prompt();
 
-  deferredPrompt.userChoice.then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the install prompt');
-    } else {
-      console.log('User dismissed the install prompt');
-    }
-    deferredPrompt = null;
-  });
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the install prompt');
+        } else {
+            console.log('User dismissed the install prompt');
+        }
+        localStorage.setItem('pwaInstallPromptShown', 'true');
+        deferredPrompt = null;
+    });
 });
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js')
-    .then(() => {
-      console.log('Service Worker registered');
-    })
-    .catch((error) => {
-      console.error('Service Worker registration failed:', error);
-    });
+    navigator.serviceWorker.register('service-worker.js')
+        .then(() => {
+            console.log('Service Worker registered');
+        })
+        .catch((error) => {
+            console.error('Service Worker registration failed:', error);
+        });
 }
