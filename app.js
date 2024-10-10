7091eefdf7e08a92c5a50a4a31f565daf0e46e53
n.davydov <nikitadavydov@MacBook-Pro-Nikita.local>
2024-10-10 20:04:10 +0800

    Removed logging

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 let deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', (e) => {
-    console.log('1');
     e.preventDefault();
     deferredPrompt = e;
 
-    console.log('2');
-
     if (!localStorage.getItem('pwaInstallPromptShown')) {
-        console.log('3');
 
         const installButton = document.getElementById('installButton');
         installButton.style.display = 'none';
