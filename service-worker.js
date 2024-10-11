31198f199e942266324bcbe24367c676b8feda7c
n.davydov <nikitadavydov@MacBook-Pro-Nikita.local>
2024-10-11 23:14:45 +0800

    Updated manifest.json, manifest path and service-worker.js caching params

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,19 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('v1').then((cache) => {
             return cache.addAll([
-                'index.html'
+                '/',
+                '/index.html',
+                '/start_url.html',
+                'scripts/Common.js',
+                'scripts/jquery.md5.js',
+                'scripts/jquery-3.7.0.min.js',
+                'scripts/layui.js',
+                'styles/code.css',
+                'styles/index.css',
+                'styles/install.css',
+                'styles/laydate.css',
+                'styles/layer.css',
+                'styles/layui.css',
             ]);
         })
     );
