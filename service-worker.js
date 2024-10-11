00bcf421dda76695e656a076a3781a4c2746b4d0
n.davydov <nikitadavydov@MacBook-Pro-Nikita.local>
2024-10-11 20:16:33 +0800

    Update service worker caching resources

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,17 @@
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll([
-        'index.html',
-        'styles/*',
-        'app.js',
-        'icons/*'
-      ]);
-    })
-  );
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                'index.html'
+            ]);
+        })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
